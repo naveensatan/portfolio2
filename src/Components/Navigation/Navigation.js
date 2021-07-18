4e479cba088d0fee1864fbc70ef4ad075dde7070
Naveen Satanarachchi <101419432@student.swin.edu.au>
2021-07-19 00:20:06 +1000

    responsive (375px-iphoneX)

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,9 +6,9 @@ const Navigation = () => (
     <nav> 
             <div className = "nav-links" >
                 <ul >
-                    <li > <Link to = "articles" spy="true" smooth="true"> Articles </Link> </li> 
-                    <li > <Link to = "about" spy="true" smooth="true"> About </Link> </li>
-                    <li > <Link to = "portfolio" spy="true" smooth="true"> Portfolio </Link> </li>
+                    <li > <Link to = "articles" smooth="true"> Articles </Link> </li> 
+                    <li > <Link to = "about" smooth="true"> About </Link> </li>
+                    <li > <Link to = "portfolio" smooth="true"> Portfolio </Link> </li>
                 </ul> 
             </div> 
     </nav>
